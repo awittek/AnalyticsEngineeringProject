0963fc7738c7624653e24b9857d40077320c73f9
Agata Wit <agatawit@MacBook-Air-Agata.local>
2023-04-10 17:59:11 +0200

    Add models to reporting layer

--- olympics/models/reporting/rpt_olympics_games.sql
+++ olympics/models/reporting/rpt_olympics_games.sql
@@ -1,32 +1,9 @@
 {{ config(materialized='table') }}
 
-WITH stg_summer_and_winter_games AS (
+WITH summer_and_winter_games_overview AS (
     SELECT * FROM {{ ref('stg_summer_games') }}
     UNION ALL
     SELECT * FROM {{ ref('stg_winter_games') }}
-),
-
-games_pivoted_by_medal AS (
-    SELECT * FROM (
-        SELECT
-            name,
-            gender,
-            age,
-            team,
-            noc,
-            games,
-            year,
-            season, 
-            city,
-            sport,
-            event,
-            medal
-        FROM stg_summer_and_winter_games
-        WHERE medal IS NOT NULL
-        )PIVOT
-        (
-        COUNT(medal) FOR medal IN ('Gold' as gold_medal,'Silver' AS silver_medal ,'Bronze' AS bronze_medal)
-        )
 )
 
-SELECT * FROM games_pivoted_by_medal
+SELECT * FROM summer_and_winter_games_overview
---
+++ olympics/models/reporting/rpt_olympics_medal_overview.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='table') }}
+
+WITH olympics_games_pivoted_by_medal AS (
+    SELECT * FROM (
+        SELECT
+            name,
+            gender,
+            age,
+            team,
+            noc,
+            games,
+            year,
+            season, 
+            city,
+            sport,
+            event,
+            medal
+        FROM {{ ref('rpt_olympics_games') }}
+        WHERE medal IS NOT NULL
+        )PIVOT
+        (
+        COUNT(medal) FOR medal IN ('Gold' AS gold_medal, 'Silver' AS silver_medal, 'Bronze' AS bronze_medal)
+        )
+)
+
+SELECT * FROM olympics_games_pivoted_by_medal
