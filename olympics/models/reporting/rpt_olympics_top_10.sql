48d22722c4fba7362c052a1325f7c625dc5bbdba
Agata Wit <agatawit@MacBook-Air-Agata.local>
2023-08-09 21:23:35 +0200

    Added model rpt_olympics_top_10 to reporting layer

---
+++ olympics/models/reporting/rpt_olympics_top_10.sql
@@ -0,0 +1,22 @@
+{{ config(
+    materialized='view',
+     tags=["olympics"]
+) }}
+
+WITH rpt_olympics_medal_overview AS (
+    SELECT * FROM {{ ref('rpt_olympics_medal_overview') }}
+),
+
+rpt_olympics_top_10 AS (
+    SELECT 
+        team AS country,
+        gender,
+        sport,
+        SUM(gold_medal_total) + SUM(silver_medal_total) + SUM(bronze_medal_total) AS medal_total
+    FROM rpt_olympics_medal_overview
+    GROUP BY team, gender, sport
+    ORDER BY medal_total DESC
+    LIMIT 10
+)
+
+SELECT * FROM rpt_olympics_top_10
